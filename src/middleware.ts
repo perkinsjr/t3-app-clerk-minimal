03999f8d20be26f0ba7265c7d3fa493c886f6ac8
James Perkins <jamesperkins@hey.com>
2023-04-01 11:06:17 -0400

    update to middleware page protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,30 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export default withClerkMiddleware((_req: NextRequest) => {
+
+const publicPaths = ["/", "/sign-in*", "/sign-up*"];
+
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+
+export default withClerkMiddleware((request: NextRequest) => {
+  if (isPublic(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(request);
+
+  if (!userId) {
+    // redirect the users to /pages/sign-in/[[...index]].ts
+
+    const signInUrl = new URL("/sign-in", request.url);
+    signInUrl.searchParams.set("redirect_url", request.url);
+    return NextResponse.redirect(signInUrl);
+  }
   return NextResponse.next();
 });
 
@@ -16,5 +38,6 @@ export const config = {
      * - favicon.ico (favicon file)
      */
     "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
+    "/"
   ],
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,16 @@
 import { type AppType } from "next/app";
-import { useRouter } from "next/router";
 import {
   ClerkProvider,
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
 } from "@clerk/nextjs";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
-const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[...index]]"];
-
 const MyApp: AppType = ({ Component, pageProps }) => {
-  const router = useRouter();
+
   return (
     <ClerkProvider {...pageProps}>
-      {publicPages.includes(router.pathname) ? (
-        <Component {...pageProps} />
-      ) : (
-        <>
-          <SignedIn>
-            <Component {...pageProps} />
-          </SignedIn>
-          <SignedOut>
-            <RedirectToSignIn />
-          </SignedOut>
-        </>
-      )}
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
